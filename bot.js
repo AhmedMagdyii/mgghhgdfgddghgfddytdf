6bfab95d9e645e48973cf368586d1ab3acc7470d
gmzglmz;glmz;gm <44279195+AhmedMagdyii@users.noreply.github.com>
2018-11-23 23:50:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ const prefix = '*'
 
  client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame("Type : *mhelp || *minv", "https://www.twitch.tv/idk");
 });
  
  
