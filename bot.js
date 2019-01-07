1f644bc97e60d0cfaf18794c87a9ab4033494de3
gmzglmz;glmz;gm <44279195+AhmedMagdyii@users.noreply.github.com>
2019-01-07 12:50:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const UserBlocked = new Set();
-const prefix = '14'
+const prefix = '1'
 
  client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
